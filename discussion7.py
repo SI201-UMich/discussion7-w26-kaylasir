2f748019eab94138b2b3fec9f8113c773f899433
Kayla Sirefman <ksirefman@MacBook-Air-1540.local>
2026-02-25 17:29:08 -0500

    Task 1 and 2

--- discussion7.py
+++ discussion7.py
@@ -31,7 +31,17 @@ def load_listings(f):
     full_path = os.path.join(base_path, f)
 
     # TODO: Read the CSV using csv.reader and convert it to a list a dictionaries
-    pass
+    
+    with open(full_path, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        all_listings = []
+        for row in reader:
+            listing = {}
+            for i, column in enumerate(header):
+                listing[column] = row[i]
+            all_listings.append(listing)
+        return all_listings
 
 ###############################################################################
 ##### TASK 2: CALCULATION FUNCTION (single calculation)
@@ -51,7 +61,28 @@ def calculate_avg_price_by_neighbourhood_group_and_room(listings):
         dict mapping (neighbourhood_group, room_type) -> average_price (float)
         e.g. { ('Downtown', 'Entire home/apt'): 123.45, ... }
     """
-    pass
+    price_sum = {}
+    price_count = {}
+    for listing in listings:
+        neighbourhood_group = listing["neighbourhood_group"]
+        room_type = listing["room_type"]
+        price = float(listing["price"])
+
+        listing_key = (neighbourhood_group, room_type)
+        if listing_key in price_sum:
+            price_sum[listing_key] += price
+            price_count[listing_key] += 1
+        else:
+            price_sum[listing_key] = price
+            price_count[listing_key] = 1
+
+    avg_price = {}
+    for key in price_sum:
+        avg_price[key] = price_sum[key] / price_count[key]
+    return avg_price
+
+
+
 
 
 
@@ -73,7 +104,7 @@ def write_summary_csv(out_filename, avg_prices):
         None
             Writes a CSV file with header: neighbourhood_group, room_type, average_price
     """
-    pass
+    
 
 ###############################################################################
 ##### UNIT TESTS (Do not modify the code below!)
